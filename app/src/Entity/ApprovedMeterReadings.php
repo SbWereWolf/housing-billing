1d31d6fe6215602decb744555c77e94fc255fe48
SbWereWolf <ulfnew@gmail.com>
2023-08-18 04:59:06 +0500

    Integrate columns for ApprovedMeterReadings
    Integrate columns for ApprovedMeterReadings:
    - billing period (year, month)
    - raw readings
    - product
    - distributor
    - testing set
    - testing run

--- app/src/Entity/ApprovedMeterReadings.php
+++ app/src/Entity/ApprovedMeterReadings.php
@@ -2,8 +2,16 @@
 
 namespace App\Entity;
 
-use Doctrine\DBAL\Types\Types;
+use App\Entity\Column\HasBillingPeriod;
+use App\Entity\Column\HasDistributionPoint;
+use App\Entity\Column\HasDistributor;
+use App\Entity\Column\HasProduct;
+use App\Entity\Column\HasRawReadings;
+use App\Entity\Column\HasTestingRun;
+use App\Entity\Column\HasTestingSet;
 use Doctrine\ORM\Mapping as ORM;
+use JetBrains\PhpStorm\Pure;
+use Stringable;
 
 /**
  * ApprovedMeterReadings
@@ -11,8 +19,16 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="approved_meter_readings", uniqueConstraints={@ORM\UniqueConstraint(name="approved_meter_readings_product_distributor_year_month_run_ux", columns={"product_id", "distributor_id", "distribution_point_id", "year", "month", "testing_run_id", "raw_readings_id"})})
  * @ORM\Entity(repositoryClass="App\Repository\ApprovedMeterReadingsRepository")
  */
-class ApprovedMeterReadings
+class ApprovedMeterReadings implements Stringable
 {
+    use HasRawReadings;
+    use HasTestingRun;
+    use HasTestingSet;
+    use HasBillingPeriod;
+    use HasProduct;
+    use HasDistributor;
+    use HasDistributionPoint;
+
     /**
      * @var int
      *
@@ -23,162 +39,18 @@ class ApprovedMeterReadings
      */
     private $id;
 
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="raw_readings_id", type="bigint", nullable=true)
-     */
-    private $rawReadingsId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="testing_run_id", type="bigint", nullable=true)
-     */
-    private $testingRunId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="testing_set_id", type="bigint", nullable=true)
-     */
-    private $testingSetId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="year", type="integer", nullable=true)
-     */
-    private $year;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="month", type="integer", nullable=true)
-     */
-    private $month;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="product_id", type="bigint", nullable=true)
-     */
-    private $productId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="distributor_id", type="bigint", nullable=true)
-     */
-    private $distributorId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="distribution_point_id", type="bigint", nullable=true)
-     */
-    private $distributionPointId;
-
     public function getId(): ?string
     {
         return $this->id;
     }
 
-    public function getRawReadingsId(): ?string
+    #[Pure]
+    public function __toString(): string
     {
-        return $this->rawReadingsId;
+        $rr = $this->getRawReadings();
+        $tr = $this->getTestingRun();
+        $p = $this->getProduct();
+        $dp = $this->getDistributionPoint();
+        return "{$rr} ({$p}: {$dp}), {$tr}";
     }
-
-    public function setRawReadingsId(?string $rawReadingsId): static
-    {
-        $this->rawReadingsId = $rawReadingsId;
-
-        return $this;
-    }
-
-    public function getTestingRunId(): ?string
-    {
-        return $this->testingRunId;
-    }
-
-    public function setTestingRunId(?string $testingRunId): static
-    {
-        $this->testingRunId = $testingRunId;
-
-        return $this;
-    }
-
-    public function getTestingSetId(): ?string
-    {
-        return $this->testingSetId;
-    }
-
-    public function setTestingSetId(?string $testingSetId): static
-    {
-        $this->testingSetId = $testingSetId;
-
-        return $this;
-    }
-
-    public function getYear(): ?int
-    {
-        return $this->year;
-    }
-
-    public function setYear(?int $year): static
-    {
-        $this->year = $year;
-
-        return $this;
-    }
-
-    public function getMonth(): ?int
-    {
-        return $this->month;
-    }
-
-    public function setMonth(?int $month): static
-    {
-        $this->month = $month;
-
-        return $this;
-    }
-
-    public function getProductId(): ?string
-    {
-        return $this->productId;
-    }
-
-    public function setProductId(?string $productId): static
-    {
-        $this->productId = $productId;
-
-        return $this;
-    }
-
-    public function getDistributorId(): ?string
-    {
-        return $this->distributorId;
-    }
-
-    public function setDistributorId(?string $distributorId): static
-    {
-        $this->distributorId = $distributorId;
-
-        return $this;
-    }
-
-    public function getDistributionPointId(): ?string
-    {
-        return $this->distributionPointId;
-    }
-
-    public function setDistributionPointId(?string $distributionPointId): static
-    {
-        $this->distributionPointId = $distributionPointId;
-
-        return $this;
-    }
-
-
 }
