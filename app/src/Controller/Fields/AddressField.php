7583d9603bfb4c60c0ae7b072298cc1560a50422
SbWereWolf <ulfnew@gmail.com>
2023-08-17 20:05:18 +0500

    Add Address and Distribution Point fields

---
+++ app/src/Controller/Fields/AddressField.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Fields;
+
+use App\Controller\Admin\AddressCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Filter\FilterInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+
+class AddressField
+{
+    public static function getFilter(): FilterInterface
+    {
+        return EntityFilter::new('address', 'Адрес');
+    }
+
+    public static function getField(): FieldInterface
+    {
+        return AssociationField::
+        new(
+            'address',
+            'Адрес'
+        )
+            ->setCrudController(AddressCrudController::class);
+    }
+}
---
+++ app/src/Controller/Fields/DistributionPointField.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Fields;
+
+use App\Controller\Admin\AddressCrudController;
+use App\Controller\Admin\DistributionPointCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Filter\FilterInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+
+class DistributionPointField
+{
+    public static function getFilter(): FilterInterface
+    {
+        return EntityFilter::new(
+            'distributionPoint',
+            'Точка поставки'
+        );
+    }
+
+    public static function getField(): FieldInterface
+    {
+        return AssociationField::
+        new(
+            'distributionPoint',
+            'Точка поставки'
+        )
+            ->setCrudController(
+                DistributionPointCrudController::class
+            );
+    }
+}
