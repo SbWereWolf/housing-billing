b70307638a99c4ae40e310e5b7fa3d8d09ec2ae1
SbWereWolf <ulfnew@gmail.com>
2023-08-19 05:18:56 +0500

    Use EntityWithCaptionController for BillingOption

--- app/src/Controller/Admin/BillingOptionCrudController.php
+++ app/src/Controller/Admin/BillingOptionCrudController.php
@@ -2,24 +2,23 @@
 
 namespace App\Controller\Admin;
 
+use App\Controller\Base\EntityWithCaptionController;
 use App\Entity\BillingOption;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
-class BillingOptionCrudController extends AbstractCrudController
+class BillingOptionCrudController extends EntityWithCaptionController
 {
     public static function getEntityFqcn(): string
     {
         return BillingOption::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
+    public function configureCrud(Crud $crud): Crud
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $crud = parent::configureCrud($crud);
+
+        return $crud
+            ->setEntityLabelInPlural('Параметры начислений')
+            ->setEntityLabelInSingular('Параметр начисления');
     }
-    */
 }
