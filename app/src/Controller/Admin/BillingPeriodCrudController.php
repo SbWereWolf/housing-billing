5330b9569eddee117b22cf0042bf78a330dacee9
SbWereWolf <ulfnew@gmail.com>
2023-08-20 03:13:26 +0500

    Configure BillingPeriodCrudController

--- app/src/Controller/Admin/BillingPeriodCrudController.php
+++ app/src/Controller/Admin/BillingPeriodCrudController.php
@@ -2,24 +2,42 @@
 
 namespace App\Controller\Admin;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\BillingPeriodField;
 use App\Entity\BillingPeriod;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\DateTimeFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
 
-class BillingPeriodCrudController extends AbstractCrudController
+class BillingPeriodCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return BillingPeriod::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
+    public function configureCrud(Crud $crud): Crud
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        return $crud
+            ->setEntityLabelInPlural('Расчётные периоды')
+            ->setEntityLabelInSingular('Расчётный период')
+            ->setDefaultSort([
+                'year' => 'ASC',
+                'month' => 'ASC',
+                'start' => 'ASC',
+                'id' => 'ASC',
+            ]);
     }
-    */
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(BillingPeriodField::getYearFilter())
+            ->add(BillingPeriodField::getMonthFilter())
+            ->add(TextFilter::new('title','Название'))
+            ->add(DateTimeFilter::new('start','Начало'))
+            ->add(DateTimeFilter::new('finish','Окончание'))
+            ;
+    }
 }
