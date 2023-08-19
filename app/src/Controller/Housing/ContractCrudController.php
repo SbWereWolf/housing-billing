043f21822cd31d6559d576f5f272e497d5139ec8
SbWereWolf <ulfnew@gmail.com>
2023-08-20 04:41:15 +0500

    Use customer field for Contract entity

--- app/src/Controller/Housing/ContractCrudController.php
+++ app/src/Controller/Housing/ContractCrudController.php
@@ -2,24 +2,42 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\CustomerField;
 use App\Entity\Contract;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class ContractCrudController extends AbstractCrudController
+class ContractCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return Contract::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Договоры')
+            ->setEntityLabelInSingular('Договор')
+            ->setDefaultSort([
+                'customer' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(CustomerField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = $this->getBaseFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield CustomerField::getField();
     }
-    */
 }
