0be328a3e81597313abbb7f7d01d99755e357052
SbWereWolf <ulfnew@gmail.com>
2023-08-19 06:11:02 +0500

    Add menu items for simple entities
    Add menu items for simple entities:
    - Customer
    - DeviceOption
    - Distributor
    - LegalEntityOption
    - LocationOption
    - MeasuringScale
    - MeteringDeviceModel
    - NaturalPersonOption
    - PersonalAccountOption
    - Product
    - ReadingsPurpose
    - ReadingsSender
    - ReadingsWay
    - TestingRule
    - TestingSet
    - UnitsOfMeasure

--- app/src/Controller/Admin/DashboardController.php
+++ app/src/Controller/Admin/DashboardController.php
@@ -7,6 +7,22 @@ use App\Entity\AddressDistributionPoint;
 use App\Entity\AddressLocationOption;
 use App\Entity\ApprovedMeterReadings;
 use App\Entity\BillingOption;
+use App\Entity\Customer;
+use App\Entity\DeviceOption;
+use App\Entity\Distributor;
+use App\Entity\LegalEntityOption;
+use App\Entity\LocationOption;
+use App\Entity\MeasuringScale;
+use App\Entity\MeteringDeviceModel;
+use App\Entity\NaturalPersonOption;
+use App\Entity\PersonalAccountOption;
+use App\Entity\Product;
+use App\Entity\ReadingsPurpose;
+use App\Entity\ReadingsSender;
+use App\Entity\ReadingsWay;
+use App\Entity\TestingRule;
+use App\Entity\TestingSet;
+use App\Entity\UnitsOfMeasure;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -71,6 +87,86 @@ class DashboardController extends AbstractDashboardController
             'Параметры начислений',
             'fas fa-list',
             BillingOption::class
+        );
+        yield MenuItem::linkToCrud(
+            'Услуги ЖКХ',
+            'fas fa-list',
+            Product::class
+        );
+        yield MenuItem::linkToCrud(
+            'Назначения показаний',
+            'fas fa-list',
+            ReadingsPurpose::class
+        );
+        yield MenuItem::linkToCrud(
+            'Отправители показаний',
+            'fas fa-list',
+            ReadingsSender::class
+        );
+        yield MenuItem::linkToCrud(
+            'Каналы поступления показаний',
+            'fas fa-list',
+            ReadingsWay::class
+        );
+        yield MenuItem::linkToCrud(
+            'Правила проверки показаний',
+            'fas fa-list',
+            TestingRule::class
+        );
+        yield MenuItem::linkToCrud(
+            'Наборы правил для проверки показаний',
+            'fas fa-list',
+            TestingSet::class
+        );
+        yield MenuItem::linkToCrud(
+            'Единицы измерения',
+            'fas fa-list',
+            UnitsOfMeasure::class
+        );
+        yield MenuItem::linkToCrud(
+            'Потребители услуг ЖКХ',
+            'fas fa-list',
+            Customer::class
+        );
+        yield MenuItem::linkToCrud(
+            'Параметры приборов учёта',
+            'fas fa-list',
+            DeviceOption::class
+        );
+        yield MenuItem::linkToCrud(
+            'Поставщики услуг ЖКХ',
+            'fas fa-list',
+            Distributor::class
+        );
+        yield MenuItem::linkToCrud(
+            'Параметры юридических лиц',
+            'fas fa-list',
+            LegalEntityOption::class
+        );
+        yield MenuItem::linkToCrud(
+            'Параметры места',
+            'fas fa-list',
+            LocationOption::class
+        );
+        yield MenuItem::linkToCrud(
+            'Шкалы измерений',
+            'fas fa-list',
+            MeasuringScale::class
+        );
+        yield MenuItem::linkToCrud(
+            'Модели приборов учёта',
+            'fas fa-list',
+            MeteringDeviceModel::class
+        );
+        yield MenuItem::linkToCrud(
+            'Параметры физических лиц',
+            'fas fa-list',
+            NaturalPersonOption::class
+        );
+        yield MenuItem::linkToCrud(
+            'Параметры лицевых счетов',
+            'fas fa-list',
+            PersonalAccountOption::class
         );
     }
 }
