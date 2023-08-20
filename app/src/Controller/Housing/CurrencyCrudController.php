cbfecc17b58d12f5ba5739053308c1172b195cec
SbWereWolf <ulfnew@gmail.com>
2023-08-21 03:35:51 +0500

    Use field UnitsOfMeasure for Currency

--- app/src/Controller/Housing/CurrencyCrudController.php
+++ app/src/Controller/Housing/CurrencyCrudController.php
@@ -2,24 +2,43 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\UnitsOfMeasureField;
 use App\Entity\Currency;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class CurrencyCrudController extends AbstractCrudController
+class CurrencyCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return Currency::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Валюты')
+            ->setEntityLabelInSingular('Валюта')
+            ->setDefaultSort([
+                'unitsOfMeasure' => 'ASC',
+                'id' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(UnitsOfMeasureField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = $this->getBaseFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield UnitsOfMeasureField::getField();
     }
-    */
 }
