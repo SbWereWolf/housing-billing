7f21930951323a7028b24a37286c4e2c86404efb
SbWereWolf <ulfnew@gmail.com>
2023-08-21 04:13:32 +0500

    Use field for NaturalPersonOption
    Use field NaturalPersonOption for CustomerNaturalPersonOptionCrudController

--- app/src/Controller/Housing/CustomerNaturalPersonOptionCrudController.php
+++ app/src/Controller/Housing/CustomerNaturalPersonOptionCrudController.php
@@ -2,24 +2,55 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\BillingOptionField;
+use App\Controller\Fields\CustomerField;
+use App\Controller\Fields\PersonalOptionField;
 use App\Entity\CustomerNaturalPersonOption;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class CustomerNaturalPersonOptionCrudController extends AbstractCrudController
+class CustomerNaturalPersonOptionCrudController
+    extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return CustomerNaturalPersonOption::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Параметры начислений для физических лиц'
+            )
+            ->setEntityLabelInSingular(
+                'Параметр начислений для физических лиц'
+            )
+            ->setDefaultSort([
+                'customer' => 'ASC',
+                'personOption' => 'ASC',
+                'billingOption' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(CustomerField::getFilter())
+            ->add(PersonalOptionField::getFilter())
+            ->add(BillingOptionField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = $this->getBaseFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield CustomerField::getField();
+        yield PersonalOptionField::getField();
+        yield BillingOptionField::getField();
     }
-    */
 }
