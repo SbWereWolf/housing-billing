fefa978cb6023f00a29f0e83802b8aef82412e77
SbWereWolf <ulfnew@gmail.com>
2023-08-20 05:52:20 +0500

    Use field for ContractPersonalAccountOption form
    Use field for ContractPersonalAccountOption form:
    - Contract
    - Account
    - Account Option
    - Billing Option

--- app/src/Controller/Housing/ContractPersonalAccountOptionCrudController.php
+++ app/src/Controller/Housing/ContractPersonalAccountOptionCrudController.php
@@ -2,24 +2,59 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\AccountField;
+use App\Controller\Fields\AccountOptionField;
+use App\Controller\Fields\BillingOptionField;
+use App\Controller\Fields\ContractField;
 use App\Entity\ContractPersonalAccountOption;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class ContractPersonalAccountOptionCrudController extends AbstractCrudController
+class ContractPersonalAccountOptionCrudController
+    extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return ContractPersonalAccountOption::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Параметры лицевых счетов для начислений'
+            )
+            ->setEntityLabelInSingular(
+                'Параметр лицевого счета для начислений'
+            )
+            ->setDefaultSort([
+                'contract' => 'ASC',
+                'account' => 'ASC',
+                'accountOption' => 'ASC',
+                'billingOption' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(ContractField::getFilter())
+            ->add(AccountField::getFilter())
+            ->add(AccountOptionField::getFilter())
+            ->add(BillingOptionField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = $this->getBaseFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield ContractField::getField();
+        yield AccountField::getField();
+        yield AccountOptionField::getField();
+        yield BillingOptionField::getField();
     }
-    */
 }
