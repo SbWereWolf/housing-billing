bfdc4d3d712108565a11aea6e84cd9bd77e24f75
SbWereWolf <ulfnew@gmail.com>
2023-08-21 04:12:17 +0500

    Use column NaturalPersonOption
    Use column NaturalPersonOption for CustomerNaturalPersonOption

--- app/src/Entity/CustomerNaturalPersonOption.php
+++ app/src/Entity/CustomerNaturalPersonOption.php
@@ -2,7 +2,9 @@
 
 namespace App\Entity;
 
-use Doctrine\DBAL\Types\Types;
+use App\Entity\Column\HasBillingOption;
+use App\Entity\Column\HasCustomer;
+use App\Entity\Column\HasPersonOption;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -13,6 +15,10 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class CustomerNaturalPersonOption
 {
+    use HasCustomer;
+    use HasPersonOption;
+    use HasBillingOption;
+
     /**
      * @var int
      *
@@ -23,67 +29,8 @@ class CustomerNaturalPersonOption
      */
     private $id;
 
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="billing_option_id", type="bigint", nullable=true)
-     */
-    private $billingOptionId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="natural_person_option_id", type="bigint", nullable=true)
-     */
-    private $naturalPersonOptionId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="customer_id", type="bigint", nullable=true)
-     */
-    private $customerId;
-
     public function getId(): ?string
     {
         return $this->id;
     }
-
-    public function getBillingOptionId(): ?string
-    {
-        return $this->billingOptionId;
-    }
-
-    public function setBillingOptionId(?string $billingOptionId): static
-    {
-        $this->billingOptionId = $billingOptionId;
-
-        return $this;
-    }
-
-    public function getNaturalPersonOptionId(): ?string
-    {
-        return $this->naturalPersonOptionId;
-    }
-
-    public function setNaturalPersonOptionId(?string $naturalPersonOptionId): static
-    {
-        $this->naturalPersonOptionId = $naturalPersonOptionId;
-
-        return $this;
-    }
-
-    public function getCustomerId(): ?string
-    {
-        return $this->customerId;
-    }
-
-    public function setCustomerId(?string $customerId): static
-    {
-        $this->customerId = $customerId;
-
-        return $this;
-    }
-
-
 }
