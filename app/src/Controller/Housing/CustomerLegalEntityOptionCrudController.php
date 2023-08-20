46bb977f257bd2bd8a7c05ac5cb6e6582d533210
SbWereWolf <ulfnew@gmail.com>
2023-08-21 04:00:42 +0500

    Use field LegalEntityOption
    Use field LegalEntityOption for CustomerLegalEntityOption

--- app/src/Controller/Housing/CustomerLegalEntityOptionCrudController.php
+++ app/src/Controller/Housing/CustomerLegalEntityOptionCrudController.php
@@ -2,24 +2,55 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\BillingOptionField;
+use App\Controller\Fields\CustomerField;
+use App\Controller\Fields\EntityOptionField;
 use App\Entity\CustomerLegalEntityOption;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class CustomerLegalEntityOptionCrudController extends AbstractCrudController
+class CustomerLegalEntityOptionCrudController
+    extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return CustomerLegalEntityOption::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Параметры начислений для юридических лиц'
+            )
+            ->setEntityLabelInSingular(
+                'Параметр начислений для юридических лиц'
+            )
+            ->setDefaultSort([
+                'customer' => 'ASC',
+                'entityOption' => 'ASC',
+                'billingOption' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(CustomerField::getFilter())
+            ->add(EntityOptionField::getFilter())
+            ->add(BillingOptionField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = $this->getBaseFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield CustomerField::getField();
+        yield EntityOptionField::getField();
+        yield BillingOptionField::getField();
     }
-    */
 }
