2b3d227349ba44bc42288d13c52161d65c253b66
SbWereWolf <ulfnew@gmail.com>
2023-08-22 05:06:28 +0500

    Use fields for DeviceModelScaleCrudController
    Use fields for DeviceModelScaleCrudController:
    - model
    - scale
    - purpose
    - units

--- app/src/Controller/Housing/DeviceModelScaleCrudController.php
+++ app/src/Controller/Housing/DeviceModelScaleCrudController.php
@@ -2,24 +2,58 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\ModelField;
+use App\Controller\Fields\PurposeField;
+use App\Controller\Fields\ScaleField;
+use App\Controller\Fields\UnitsOfMeasureField;
 use App\Entity\DeviceModelScale;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class DeviceModelScaleCrudController extends AbstractCrudController
+class DeviceModelScaleCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return DeviceModelScale::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Шкалы приборов учёта'
+            )
+            ->setEntityLabelInSingular(
+                'Шкала прибора учёта'
+            )
+            ->setDefaultSort([
+                'model' => 'ASC',
+                'scale' => 'ASC',
+                'unitsOfMeasure' => 'ASC',
+                'purpose' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(ModelField::getFilter())
+            ->add(ScaleField::getFilter())
+            ->add(UnitsOfMeasureField::getFilter())
+            ->add(PurposeField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = $this->getBaseFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield ModelField::getField();
+        yield ScaleField::getField();
+        yield UnitsOfMeasureField::getField();
+        yield PurposeField::getField();
     }
-    */
 }
