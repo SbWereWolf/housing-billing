581a049580fa059f59b053438570a84ade26a259
SbWereWolf <ulfnew@gmail.com>
2023-08-22 07:02:56 +0500

    Use field customer for LegalEntity

--- app/src/Controller/Housing/LegalEntityCrudController.php
+++ app/src/Controller/Housing/LegalEntityCrudController.php
@@ -2,24 +2,42 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\CustomerField;
 use App\Entity\LegalEntity;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class LegalEntityCrudController extends AbstractCrudController
+class LegalEntityCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return LegalEntity::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Юридические лица')
+            ->setEntityLabelInSingular('Юридическое лицо')
+            ->setDefaultSort([
+                'customer' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(CustomerField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = $this->getBaseFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield CustomerField::getField();
     }
-    */
 }
