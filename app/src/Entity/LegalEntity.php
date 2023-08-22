2f7f5335ece2790885fcc03fdde34b7418f8fa28
SbWereWolf <ulfnew@gmail.com>
2023-08-22 07:02:29 +0500

    Use column customer for LegalEntity

--- app/src/Entity/LegalEntity.php
+++ app/src/Entity/LegalEntity.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use Doctrine\DBAL\Types\Types;
+use App\Entity\Column\HasCustomer;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -13,6 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class LegalEntity
 {
+    use HasCustomer;
     /**
      * @var int
      *
@@ -23,29 +24,8 @@ class LegalEntity
      */
     private $id;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="customer_id", type="bigint", nullable=false)
-     */
-    private $customerId;
-
     public function getId(): ?string
     {
         return $this->id;
     }
-
-    public function getCustomerId(): ?string
-    {
-        return $this->customerId;
-    }
-
-    public function setCustomerId(string $customerId): static
-    {
-        $this->customerId = $customerId;
-
-        return $this;
-    }
-
-
 }
