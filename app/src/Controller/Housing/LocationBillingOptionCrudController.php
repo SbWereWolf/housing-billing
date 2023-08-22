66536fcb1e8da1314faeccb24b100b5566a21567
SbWereWolf <ulfnew@gmail.com>
2023-08-22 07:17:16 +0500

    Use fields for LocationBillingOption
    Use fields for LocationBillingOption:
    - LocationOption
    - BillingOption

--- app/src/Controller/Housing/LocationBillingOptionCrudController.php
+++ app/src/Controller/Housing/LocationBillingOptionCrudController.php
@@ -2,24 +2,51 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\BillingOptionField;
+use App\Controller\Fields\LocationOptionField;
 use App\Entity\LocationBillingOption;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class LocationBillingOptionCrudController extends AbstractCrudController
+class LocationBillingOptionCrudController
+    extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return LocationBillingOption::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Параметры начислений для местонахождений'
+            )
+            ->setEntityLabelInSingular(
+                'Параметр начислений для местонахождений'
+            )
+            ->setDefaultSort([
+                'locationOption' => 'ASC',
+                'billingOption' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(LocationOptionField::getFilter())
+            ->add(BillingOptionField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = $this->getBaseFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield LocationOptionField::getField();
+        yield BillingOptionField::getField();
     }
-    */
 }
