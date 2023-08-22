ace6ae7fe4d10b793f4d5c7547cab06447ea95a9
SbWereWolf <ulfnew@gmail.com>
2023-08-23 03:30:19 +0500

    Use fields for MeterReadings form
    Use fields for MeterReadings form:
    - Product
    - Distributor
    - DistributionPoint
    - RawReadings
    - BillingPeriod
    - TestingRun

--- app/src/Controller/Housing/MeterReadingsCrudController.php
+++ app/src/Controller/Housing/MeterReadingsCrudController.php
@@ -2,24 +2,65 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\BillingPeriodField;
+use App\Controller\Fields\DistributionPointField;
+use App\Controller\Fields\DistributorField;
+use App\Controller\Fields\ProductField;
+use App\Controller\Fields\RawReadingsField;
+use App\Controller\Fields\TestingRunField;
 use App\Entity\MeterReadings;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class MeterReadingsCrudController extends AbstractCrudController
+class MeterReadingsCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return MeterReadings::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Показания приборов учёта')
+            ->setEntityLabelInSingular('Показания прибора учёта')
+            ->setDefaultSort([
+                'product' => 'ASC',
+                'distributor' => 'ASC',
+                'distributionPoint' => 'ASC',
+                'year' => 'ASC',
+                'month' => 'ASC',
+                'rawReadings' => 'ASC',
+                'testingRun' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(TestingRunField::getFilter())
+            ->add(ProductField::getFilter())
+            ->add(BillingPeriodField::getYearFilter())
+            ->add(BillingPeriodField::getMonthFilter())
+            ->add(DistributionPointField::getFilter())
+            ->add(RawReadingsField::getFilter())
+            ->add(DistributorField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield TestingRunField::getField();
+        yield ProductField::getField();
+        yield BillingPeriodField::getYearField();
+        yield BillingPeriodField::getMonthField();
+        yield DistributionPointField::getField();
+        yield RawReadingsField::getField();
+        yield DistributorField::getField();
     }
-    */
 }
