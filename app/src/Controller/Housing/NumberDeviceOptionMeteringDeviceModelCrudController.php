0d709a8074dce63016576ac5d95b72c7e8e12e4a
SbWereWolf <ulfnew@gmail.com>
2023-08-23 04:36:07 +0500

    Use fields NumberDeviceOptionMeteringDeviceModel
    Use fields for NumberDeviceOptionMeteringDeviceModel:
    - Model
    - DeviceOption

--- app/src/Controller/Housing/NumberDeviceOptionMeteringDeviceModelCrudController.php
+++ app/src/Controller/Housing/NumberDeviceOptionMeteringDeviceModelCrudController.php
@@ -2,24 +2,47 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\DeviceOptionField;
+use App\Controller\Fields\ModelField;
 use App\Entity\NumberDeviceOptionMeteringDeviceModel;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class NumberDeviceOptionMeteringDeviceModelCrudController extends AbstractCrudController
+class NumberDeviceOptionMeteringDeviceModelCrudController
+    extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return NumberDeviceOptionMeteringDeviceModel::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Приборы учёта для услуг ЖКХ')
+            ->setEntityLabelInSingular('Прибор учёта для услуги ЖКХ')
+            ->setDefaultSort([
+                'model' => 'ASC',
+                'deviceOption' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(ModelField::getFilter())
+            ->add(DeviceOptionField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield ModelField::getField();
+        yield DeviceOptionField::getField();
     }
-    */
 }
