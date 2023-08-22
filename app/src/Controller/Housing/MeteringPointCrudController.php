2390127f614aba0e7574ba0c2b62258171ff120f
SbWereWolf <ulfnew@gmail.com>
2023-08-23 03:26:50 +0500

    Use fields for MeteringPoint form
    Use fields for MeteringPoint form:
    - Address
    - Product
    - Distributor
    - DistributionPoint

--- app/src/Controller/Housing/MeteringPointCrudController.php
+++ app/src/Controller/Housing/MeteringPointCrudController.php
@@ -2,24 +2,54 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\AddressField;
+use App\Controller\Fields\DistributionPointField;
+use App\Controller\Fields\DistributorField;
+use App\Controller\Fields\ProductField;
 use App\Entity\MeteringPoint;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class MeteringPointCrudController extends AbstractCrudController
+class MeteringPointCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return MeteringPoint::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Точки учёта')
+            ->setEntityLabelInSingular('Точка учёта')
+            ->setDefaultSort([
+                'address' => 'ASC',
+                'product' => 'ASC',
+                'distributor' => 'ASC',
+                'distributionPoint' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(AddressField::getFilter())
+            ->add(ProductField::getFilter())
+            ->add(DistributorField::getFilter())
+            ->add(DistributionPointField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield AddressField::getField();
+        yield ProductField::getField();
+        yield DistributorField::getField();
+        yield DistributionPointField::getField();
     }
-    */
 }
