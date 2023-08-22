70dba23b52947b2e5618fa3b8ee28593a684d194
SbWereWolf <ulfnew@gmail.com>
2023-08-23 04:10:14 +0500

    Use fields for NaturalPersonBillingOption form
    Use fields for NaturalPersonBillingOption form:
    - PersonalOption
    - BillingOption

--- app/src/Controller/Housing/NaturalPersonBillingOptionCrudController.php
+++ app/src/Controller/Housing/NaturalPersonBillingOptionCrudController.php
@@ -2,24 +2,51 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\BillingOptionField;
+use App\Controller\Fields\PersonalOptionField;
 use App\Entity\NaturalPersonBillingOption;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class NaturalPersonBillingOptionCrudController extends AbstractCrudController
+class NaturalPersonBillingOptionCrudController
+    extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return NaturalPersonBillingOption::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Параметры начислений для физических лиц'
+            )
+            ->setEntityLabelInSingular(
+                'Параметр начислений для физических лиц'
+            )
+            ->setDefaultSort([
+                'personOption' => 'ASC',
+                'billingOption' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(PersonalOptionField::getFilter())
+            ->add(BillingOptionField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield PersonalOptionField::getField();
+        yield BillingOptionField::getField();
     }
-    */
 }
