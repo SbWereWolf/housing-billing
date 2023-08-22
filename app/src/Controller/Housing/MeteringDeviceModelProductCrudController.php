6aed5f13012972755df539d1bc37229cc519e335
SbWereWolf <ulfnew@gmail.com>
2023-08-23 03:21:05 +0500

    Use fields for MeteringDeviceModelProduct form
    Use fields for MeteringDeviceModelProduct form:
    - Product
    - Model

--- app/src/Controller/Housing/MeteringDeviceModelProductCrudController.php
+++ app/src/Controller/Housing/MeteringDeviceModelProductCrudController.php
@@ -2,24 +2,47 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\ModelField;
+use App\Controller\Fields\ProductField;
 use App\Entity\MeteringDeviceModelProduct;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class MeteringDeviceModelProductCrudController extends AbstractCrudController
+class MeteringDeviceModelProductCrudController
+    extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return MeteringDeviceModelProduct::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Приборы учёта для услуг ЖКХ')
+            ->setEntityLabelInSingular('Прибор учёта для услуги ЖКХ')
+            ->setDefaultSort([
+                'model' => 'ASC',
+                'product' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(ProductField::getFilter())
+            ->add(ModelField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield ProductField::getField();
+        yield ModelField::getField();
     }
-    */
 }
