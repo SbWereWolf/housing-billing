7cd0f99bcd3dac0ea2980c1c28efdc61dabaaf2e
SbWereWolf <ulfnew@gmail.com>
2023-08-23 04:17:43 +0500

    Use Customer field for NaturalPerson form

--- app/src/Controller/Housing/NaturalPersonCrudController.php
+++ app/src/Controller/Housing/NaturalPersonCrudController.php
@@ -2,24 +2,42 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\CustomerField;
 use App\Entity\NaturalPerson;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class NaturalPersonCrudController extends AbstractCrudController
+class NaturalPersonCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return NaturalPerson::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Физические лица')
+            ->setEntityLabelInSingular('Физическое лицо')
+            ->setDefaultSort([
+                'customer' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(CustomerField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield CustomerField::getField();
     }
-    */
 }
