b83ed47e7ebd73b1f5a350a0143179dface60632
SbWereWolf <ulfnew@gmail.com>
2023-08-22 06:52:37 +0500

    Use fields for LegalEntityBillingOption
    Use fields for LegalEntityBillingOption:
    - EntityOption
    - BillingOption

--- app/src/Controller/Housing/LegalEntityBillingOptionCrudController.php
+++ app/src/Controller/Housing/LegalEntityBillingOptionCrudController.php
@@ -2,24 +2,51 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\BillingOptionField;
+use App\Controller\Fields\EntityOptionField;
 use App\Entity\LegalEntityBillingOption;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class LegalEntityBillingOptionCrudController extends AbstractCrudController
+class LegalEntityBillingOptionCrudController
+    extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return LegalEntityBillingOption::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Параметры начислений для юридических лиц'
+            )
+            ->setEntityLabelInSingular(
+                'Параметр начислений для юридических лиц'
+            )
+            ->setDefaultSort([
+                'entityOption' => 'ASC',
+                'billingOption' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(EntityOptionField::getFilter())
+            ->add(BillingOptionField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = $this->getBaseFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield EntityOptionField::getField();
+        yield BillingOptionField::getField();
     }
-    */
 }
