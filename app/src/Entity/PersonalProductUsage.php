d43bef30a65d9ae4e5fe5b09eb32ab8b5e305ced
SbWereWolf <ulfnew@gmail.com>
2023-08-23 06:48:50 +0500

    Use columns for PersonalProductUsage entity
    Use columns for PersonalProductUsage entity:
    - Product
    - Distributor
    - DistributionPoint
    - BillingPeriod
    - Account
    - UnitsOfMeasure
    - Address
    - Customer

--- app/src/Entity/PersonalProductUsage.php
+++ app/src/Entity/PersonalProductUsage.php
@@ -2,7 +2,14 @@
 
 namespace App\Entity;
 
-use Doctrine\DBAL\Types\Types;
+use App\Entity\Column\HasAccount;
+use App\Entity\Column\HasAddress;
+use App\Entity\Column\HasBillingPeriod;
+use App\Entity\Column\HasCustomer;
+use App\Entity\Column\HasDistributionPoint;
+use App\Entity\Column\HasDistributor;
+use App\Entity\Column\HasProduct;
+use App\Entity\Column\HasUnitsOfMeasure;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -13,6 +20,15 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class PersonalProductUsage
 {
+    use HasProduct;
+    use HasDistributor;
+    use HasDistributionPoint;
+    use HasBillingPeriod;
+    use HasAccount;
+    use HasUnitsOfMeasure;
+    use HasAddress;
+    use HasCustomer;
+
     /**
      * @var int
      *
@@ -26,148 +42,13 @@ class PersonalProductUsage
     /**
      * @var int|null
      *
-     * @ORM\Column(name="product_id", type="bigint", nullable=true)
-     */
-    private $productId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="distributor_id", type="bigint", nullable=true)
-     */
-    private $distributorId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="distribution_point_id", type="bigint", nullable=true)
-     */
-    private $distributionPointId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="year", type="integer", nullable=true)
-     */
-    private $year;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="month", type="integer", nullable=true)
-     */
-    private $month;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="personal_account_id", type="bigint", nullable=true)
-     */
-    private $personalAccountId;
-
-    /**
-     * @var int|null
-     *
      * @ORM\Column(name="volume", type="bigint", nullable=true)
      */
     private $volume;
 
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="units_of_measure_id", type="bigint", nullable=true)
-     */
-    private $unitsOfMeasureId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="address_id", type="bigint", nullable=true)
-     */
-    private $addressId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="customer_id", type="bigint", nullable=true)
-     */
-    private $customerId;
-
     public function getId(): ?string
     {
         return $this->id;
-    }
-
-    public function getProductId(): ?string
-    {
-        return $this->productId;
-    }
-
-    public function setProductId(?string $productId): static
-    {
-        $this->productId = $productId;
-
-        return $this;
-    }
-
-    public function getDistributorId(): ?string
-    {
-        return $this->distributorId;
-    }
-
-    public function setDistributorId(?string $distributorId): static
-    {
-        $this->distributorId = $distributorId;
-
-        return $this;
-    }
-
-    public function getDistributionPointId(): ?string
-    {
-        return $this->distributionPointId;
-    }
-
-    public function setDistributionPointId(?string $distributionPointId): static
-    {
-        $this->distributionPointId = $distributionPointId;
-
-        return $this;
-    }
-
-    public function getYear(): ?int
-    {
-        return $this->year;
-    }
-
-    public function setYear(?int $year): static
-    {
-        $this->year = $year;
-
-        return $this;
-    }
-
-    public function getMonth(): ?int
-    {
-        return $this->month;
-    }
-
-    public function setMonth(?int $month): static
-    {
-        $this->month = $month;
-
-        return $this;
-    }
-
-    public function getPersonalAccountId(): ?string
-    {
-        return $this->personalAccountId;
-    }
-
-    public function setPersonalAccountId(?string $personalAccountId): static
-    {
-        $this->personalAccountId = $personalAccountId;
-
-        return $this;
     }
 
     public function getVolume(): ?string
@@ -181,42 +62,4 @@ class PersonalProductUsage
 
         return $this;
     }
-
-    public function getUnitsOfMeasureId(): ?string
-    {
-        return $this->unitsOfMeasureId;
-    }
-
-    public function setUnitsOfMeasureId(?string $unitsOfMeasureId): static
-    {
-        $this->unitsOfMeasureId = $unitsOfMeasureId;
-
-        return $this;
-    }
-
-    public function getAddressId(): ?string
-    {
-        return $this->addressId;
-    }
-
-    public function setAddressId(?string $addressId): static
-    {
-        $this->addressId = $addressId;
-
-        return $this;
-    }
-
-    public function getCustomerId(): ?string
-    {
-        return $this->customerId;
-    }
-
-    public function setCustomerId(?string $customerId): static
-    {
-        $this->customerId = $customerId;
-
-        return $this;
-    }
-
-
 }
