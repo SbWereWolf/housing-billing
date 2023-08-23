7ac54b67182f266391768c20ad38f821221188f9
SbWereWolf <ulfnew@gmail.com>
2023-08-24 04:34:22 +0500

    Use fields for ProductDistributor form
    Use fields for ProductDistributor form:
    - Product
    - Distributor

--- app/src/Controller/Housing/ProductDistributorCrudController.php
+++ app/src/Controller/Housing/ProductDistributorCrudController.php
@@ -2,24 +2,50 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\DistributorField;
+use App\Controller\Fields\ProductField;
 use App\Entity\ProductDistributor;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class ProductDistributorCrudController extends AbstractCrudController
+class ProductDistributorCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return ProductDistributor::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Услуги поставщиков'
+            )
+            ->setEntityLabelInSingular(
+                'Услуга поставщика'
+            )
+            ->setDefaultSort([
+                'product' => 'ASC',
+                'distributor' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(ProductField::getFilter())
+            ->add(DistributorField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield ProductField::getField();
+        yield DistributorField::getField();
     }
-    */
 }
