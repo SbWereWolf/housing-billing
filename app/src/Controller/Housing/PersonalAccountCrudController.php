0c0ecce50a38465d3a364e82f63174c2999f41bb
SbWereWolf <ulfnew@gmail.com>
2023-08-23 05:05:57 +0500

    Use fields for PersonalAccount form
    Use fields for PersonalAccount form:
    - Customer
    - Contract

--- app/src/Controller/Housing/PersonalAccountCrudController.php
+++ app/src/Controller/Housing/PersonalAccountCrudController.php
@@ -2,24 +2,56 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\ContractField;
+use App\Controller\Fields\CustomerField;
 use App\Entity\PersonalAccount;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
 
-class PersonalAccountCrudController extends AbstractCrudController
+class PersonalAccountCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return PersonalAccount::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Лицевые счета')
+            ->setEntityLabelInSingular('Лицевой счет')
+            ->setDefaultSort([
+                'customer' => 'ASC',
+                'contract' => 'ASC',
+                'publicAccount' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(CustomerField::getFilter())
+            ->add(ContractField::getFilter())
+            ->add(
+                TextFilter::new(
+                    'publicAccount',
+                    'Номер лицевого счёта'
+                )
+            );
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield CustomerField::getField();
+        yield ContractField::getField();
+        yield TextField::new('publicAccount', 'Номер лицевого счёта');
     }
-    */
 }
