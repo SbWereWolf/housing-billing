aaf6dcd0e4c5cb40e50ad7e1976a3fd26f48b4e9
SbWereWolf <ulfnew@gmail.com>
2023-08-24 05:01:08 +0500

    Use fields for ProductUnitsOfMeasure form
    Use fields for ProductUnitsOfMeasure form:
    - Product
    - UnitsOfMeasure

--- app/src/Controller/Housing/ProductUnitsOfMeasureCrudController.php
+++ app/src/Controller/Housing/ProductUnitsOfMeasureCrudController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Fields\ProductField;
+use App\Controller\Fields\UnitsOfMeasureField;
 use App\Entity\ProductUnitsOfMeasure;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProductUnitsOfMeasureCrudController extends AbstractCrudController
@@ -12,14 +16,36 @@ class ProductUnitsOfMeasureCrudController extends AbstractCrudController
         return ProductUnitsOfMeasure::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Единицы измерения для услуг ЖКХ'
+            )
+            ->setEntityLabelInSingular(
+                'Единица измерения для услуги ЖКХ'
+            )
+            ->setDefaultSort([
+                'product' => 'ASC',
+                'unitsOfMeasure' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(ProductField::getFilter())
+            ->add(UnitsOfMeasureField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield ProductField::getField();
+        yield UnitsOfMeasureField::getField();
     }
-    */
 }
