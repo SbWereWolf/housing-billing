375eb94cd4b4890e20e7ca47f9fff45538ea40fd
SbWereWolf <ulfnew@gmail.com>
2023-08-24 07:43:03 +0500

    Configure SharedProduct form

--- app/src/Controller/Housing/SharedProductCrudController.php
+++ app/src/Controller/Housing/SharedProductCrudController.php
@@ -2,24 +2,68 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
 use App\Entity\SharedProduct;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
-class SharedProductCrudController extends AbstractCrudController
+class SharedProductCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return SharedProduct::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Услуги по содержанию общего имущества'
+            )
+            ->setEntityLabelInSingular(
+                'Услуга по содержанию общего имущества'
+            )
+            ->setDefaultSort([
+                'shared' => 'ASC',
+                'individual' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(
+                EntityFilter::new(
+                    'shared',
+                    'Услуга по содержанию общего имущества'
+                )
+            )
+            ->add(
+                EntityFilter::new(
+                    'individual',
+                    'Связанная услуга для собственника'
+                )
+            );
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield AssociationField::new(
+            'shared',
+            'Услуга по содержанию общего имущества'
+        )
+            ->setCrudController(ProductCrudController::class);
+        yield AssociationField::new(
+            'individual',
+            'Связанная услуга для собственника'
+        )
+            ->setCrudController(ProductCrudController::class);
     }
-    */
 }
