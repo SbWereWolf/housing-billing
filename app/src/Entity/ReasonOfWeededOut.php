2c75f1dbe4ef99e715ab6874e1ea717fc639812f
SbWereWolf <ulfnew@gmail.com>
2023-08-24 07:14:11 +0500

    Use columns for ReasonOfWeededOut entity
    Use columns for ReasonOfWeededOut entity:
    - TestingRun
    - TestingSet
    - TestingRule

--- app/src/Entity/ReasonOfWeededOut.php
+++ app/src/Entity/ReasonOfWeededOut.php
@@ -2,7 +2,9 @@
 
 namespace App\Entity;
 
-use Doctrine\DBAL\Types\Types;
+use App\Entity\Column\HasTestingRule;
+use App\Entity\Column\HasTestingRun;
+use App\Entity\Column\HasTestingSet;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -13,6 +15,10 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class ReasonOfWeededOut
 {
+    use HasTestingRun;
+    use HasTestingSet;
+    use HasTestingRule;
+
     /**
      * @var int
      *
@@ -23,67 +29,8 @@ class ReasonOfWeededOut
      */
     private $id;
 
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="testing_run_id", type="bigint", nullable=true)
-     */
-    private $testingRunId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="testing_set_id", type="bigint", nullable=true)
-     */
-    private $testingSetId;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="testing_rule_id", type="bigint", nullable=true)
-     */
-    private $testingRuleId;
-
     public function getId(): ?string
     {
         return $this->id;
     }
-
-    public function getTestingRunId(): ?string
-    {
-        return $this->testingRunId;
-    }
-
-    public function setTestingRunId(?string $testingRunId): static
-    {
-        $this->testingRunId = $testingRunId;
-
-        return $this;
-    }
-
-    public function getTestingSetId(): ?string
-    {
-        return $this->testingSetId;
-    }
-
-    public function setTestingSetId(?string $testingSetId): static
-    {
-        $this->testingSetId = $testingSetId;
-
-        return $this;
-    }
-
-    public function getTestingRuleId(): ?string
-    {
-        return $this->testingRuleId;
-    }
-
-    public function setTestingRuleId(?string $testingRuleId): static
-    {
-        $this->testingRuleId = $testingRuleId;
-
-        return $this;
-    }
-
-
 }
