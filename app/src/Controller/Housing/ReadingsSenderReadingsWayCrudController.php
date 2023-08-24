57ccd1eb30235f2449be0e67a844e356aa578a52
SbWereWolf <ulfnew@gmail.com>
2023-08-24 07:00:00 +0500

    Use fields for ReadingsSenderReadingsWay form
    Use fields for ReadingsSenderReadingsWay form:
    - Sender
    - Way

--- app/src/Controller/Housing/ReadingsSenderReadingsWayCrudController.php
+++ app/src/Controller/Housing/ReadingsSenderReadingsWayCrudController.php
@@ -2,24 +2,51 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\SenderField;
+use App\Controller\Fields\WayField;
 use App\Entity\ReadingsSenderReadingsWay;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class ReadingsSenderReadingsWayCrudController extends AbstractCrudController
+class ReadingsSenderReadingsWayCrudController
+    extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return ReadingsSenderReadingsWay::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Способы отправки показаний для отправителей'
+            )
+            ->setEntityLabelInSingular(
+                'Способ отправки показаний для отправителя'
+            )
+            ->setDefaultSort([
+                'sender' => 'ASC',
+                'way' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(SenderField::getFilter())
+            ->add(WayField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield SenderField::getField();
+        yield WayField::getField();
     }
-    */
 }
