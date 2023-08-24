e23f1b4ab0b78f5265b3f474c0356f09a80fedcb
SbWereWolf <ulfnew@gmail.com>
2023-08-24 08:03:58 +0500

    Use fields for TestingRuleSet form
    Use fields for TestingRuleSet form:
    - TestingSet
    - TestingRule

--- app/src/Controller/Housing/TestingRuleSetCrudController.php
+++ app/src/Controller/Housing/TestingRuleSetCrudController.php
@@ -2,24 +2,50 @@
 
 namespace App\Controller\Housing;
 
+use App\Controller\Base\EntityWithIdController;
+use App\Controller\Fields\TestingRuleField;
+use App\Controller\Fields\TestingSetField;
 use App\Entity\TestingRuleSet;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
-class TestingRuleSetCrudController extends AbstractCrudController
+class TestingRuleSetCrudController extends EntityWithIdController
 {
     public static function getEntityFqcn(): string
     {
         return TestingRuleSet::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural(
+                'Список правил для отбора показаний'
+            )
+            ->setEntityLabelInSingular(
+                'Пункт списка правил для отбора показаний'
+            )
+            ->setDefaultSort([
+                'testingSet' => 'ASC',
+                'rule' => 'ASC',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add(TestingSetField::getFilter())
+            ->add(TestingRuleField::getFilter());
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+        foreach ($fields as $field) {
+            yield $field;
+        }
+
+        yield TestingSetField::getField();
+        yield TestingRuleField::getField();
     }
-    */
 }
